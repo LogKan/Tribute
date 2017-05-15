bdd551d7d4d5f743253f638c5bf773d88e3b8d81
Fernando Stutz <fernando.stutz@students.fhnw.ch>
2017-05-15 17:52:25 +0200

    Warscheindlichkeit

--- src/lottoClient/source/probabilityWindow/ProbabilityModel.java
+++ src/lottoClient/source/probabilityWindow/ProbabilityModel.java
@@ -1,11 +1,80 @@
 package lottoClient.source.probabilityWindow;
 
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.util.logging.Logger;
+
 import lottoClient.source.abstractClasses.Model;
+import lottoClient.source.commonClasses.Configuration;
+import lottoClient.source.commonClasses.ServiceLocator;
 
 public class ProbabilityModel extends Model {
 	
+	ServiceLocator serviceLocator = ServiceLocator.getServiceLocator();
+	Logger logger = serviceLocator.getLogger();
+	Configuration config = serviceLocator.getConfiguration();
+	
+	// n
+	private int countLottoNumber = Integer.parseInt(config.getOption("MaxNumber"));
+	// k
+	private int countSelectedLottoNumber;
+	// n
+	private int countLottoSuperNumber = Integer.parseInt(config.getOption("MaxSuperNumber"));
+	// k
+	private int countSelectedSuperLottoNumber;
+	private BigInteger probability;
+	
+	/**
+	 * Konstruktor ProbabilityModel
+	 */
 	public ProbabilityModel(){
-		
-	}
 
+	}
+	
+	/**
+	 * Warscheindlichkeitsberechung
+	 * @param countSelectedLottoNumber
+	 * @param countSelectedSuperLottoNumber
+	 * @return String
+	 */
+	public String probability(int countSelectedLottoNumber, int countSelectedSuperLottoNumber)
+	{
+		this.countSelectedLottoNumber = countSelectedLottoNumber;
+		this.countSelectedSuperLottoNumber = countSelectedSuperLottoNumber;
+		
+		// Berechnung der Warscheindlichkeit
+		this.probability = getFactorial(this.countLottoNumber).divide(getFactorial(this.countLottoNumber-this.countSelectedLottoNumber).multiply(getFactorial(this.countSelectedLottoNumber)));
+		// Berechnung der Superzahl Warscheindlochkeit inkl. Multiplikation
+		this.probability = this.probability.multiply(getBinomial(this.countLottoSuperNumber, this.countSelectedSuperLottoNumber));
+		DecimalFormat dec = new DecimalFormat();
+		return dec.format(this.probability); 
+	}
+	
+	/**
+	 * Berechnung Fakultät n!
+	 * @author http://stackoverflow.com/questions/8992437/stackoverflowerror-computing-factorial-of-a-biginteger
+	 * @param num
+	 * @return BigInteger
+	 */
+    private static BigInteger getFactorial(int num) {
+        BigInteger fact = BigInteger.valueOf(1);
+        for (int i = 1; i <= num; i++)
+            fact = fact.multiply(BigInteger.valueOf(i));
+        return fact;
+    }
+    /**
+     * Berechnung Binomial
+     * @author http://stackoverflow.com/questions/2201113/combinatoric-n-choose-r-in-java-math
+     * @param N
+     * @param K
+     * @return BigInteger
+     */
+    private static BigInteger getBinomial(final int N, final int K) {
+        BigInteger ret = BigInteger.ONE;
+        for (int k = 0; k < K; k++) {
+            ret = ret.multiply(BigInteger.valueOf(N-k))
+                     .divide(BigInteger.valueOf(k+1));
+        }
+        return ret;
+    }
 }
--- src/lottoClient/source/probabilityWindow/ProbabilityView.java
+++ src/lottoClient/source/probabilityWindow/ProbabilityView.java
@@ -2,47 +2,77 @@ package lottoClient.source.probabilityWindow;
 
 
 import java.util.logging.Logger;
+
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import lottoClient.source.abstractClasses.View;
 import lottoClient.source.commonClasses.Configuration;
-import lottoClient.source.commonClasses.Probability;
 import lottoClient.source.commonClasses.ServiceLocator;
 
 public class ProbabilityView extends View<ProbabilityModel>{
 	
-	protected ServiceLocator serviceLocator = ServiceLocator.getServiceLocator();
-	protected Logger logger = serviceLocator.getLogger();
-	protected Configuration config = serviceLocator.getConfiguration();
+	protected ServiceLocator serviceLocator;
+	protected Logger logger;
+	protected Configuration config;
+	
+	private int countLottoNumber;
+	private int countSelectedLottoNumber;
+	private int countLottoSuperNumber;
+	private int countSelectedSuperLottoNumber;
 	
 	
 
 	protected ProbabilityView(Stage stage, ProbabilityModel model) {
 		super(stage, model);
+
 	}
 
 	@Override
 	protected Scene createGUI() {
+		serviceLocator = serviceLocator.getServiceLocator();
+		config = serviceLocator.getConfiguration();
+		
+		this.countLottoNumber = Integer.parseInt(config.getOption("MaxNumber"));
+		this.countSelectedLottoNumber = Integer.parseInt(config.getOption("SelectNumber"));
+		this.countLottoSuperNumber = Integer.parseInt(config.getOption("MaxSuperNumber"));
+		this.countSelectedSuperLottoNumber = Integer.parseInt(config.getOption("SelectSuperNumber"));
+		
+		
+		
 		BorderPane root = new BorderPane();
 		GridPane gridPane = new GridPane();
-		int counter=6*2;
+		HBox box = new HBox();
+		int counter = this.countSelectedLottoNumber * (this.countSelectedSuperLottoNumber+1);
 		
 		Label title = new Label("Lotto probability");
 		
-		for(int i=1;i<=6 ;i++){
-			for(int j=0;j<=1 ;j++){
-				gridPane.add(new Label(i+"-"+j+": "+new Probability(42, i, 6, j).toString()),0,counter--);
+		gridPane.setVgap(10);
+		gridPane.setHgap(10);
+		
+		for(int i=1;i<=this.countSelectedLottoNumber ;i++){
+			for(int j=0;j<=this.countSelectedSuperLottoNumber ;j++){
+				gridPane.add(new Label(""+i+" "), 0, counter);
+				if(j>0)
+					gridPane.add(new Label(""+j+" "), 1, counter);
+				gridPane.add(new Label("1 : "+model.probability(i, j)),3,counter--);
 			}
 		}
-
 		
 		root.setTop(title);
-		root.setCenter(gridPane);
+		box.getChildren().add(gridPane);
+		box.setAlignment(Pos.CENTER);
+		root.setAlignment(title, Pos.CENTER);
+		root.setCenter(box);
 		
-		Scene scene = new Scene(root);
+		
+		
+		Scene scene = new Scene(root,200,400);
 		return scene;
 	}
 
